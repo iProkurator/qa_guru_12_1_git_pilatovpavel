1206bc5f748771c636b490ac6b8dff242023991b
Pavel Pilatov <pilatov@gmail.com>
2022-03-31 19:26:27 +0300

    test commit

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,6 +3,6 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, QA.GURU!");
+        System.out.println("Hello, QA.GURU!!");
     }
 }
